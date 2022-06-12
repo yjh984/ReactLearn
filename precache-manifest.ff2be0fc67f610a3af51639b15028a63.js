ebd3ada0233a0b4e12560b8900916fe956571e77
Jin Yu <yjh984@daum.net>
2022-06-12 19:05:43 +0900

    Updates

---
+++ precache-manifest.ff2be0fc67f610a3af51639b15028a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c4eaffb0888e642da7f",
+    "url": "/ReactLearn/static/css/main.6aee28e0.chunk.css"
+  },
+  {
+    "revision": "3c4eaffb0888e642da7f",
+    "url": "/ReactLearn/static/js/main.3c4eaffb.chunk.js"
+  },
+  {
+    "revision": "b9d74096aa11e783ef7f",
+    "url": "/ReactLearn/static/js/1.b9d74096.chunk.js"
+  },
+  {
+    "revision": "ef9b3029ca4ecafd1aae",
+    "url": "/ReactLearn/static/js/2.ef9b3029.chunk.js"
+  },
+  {
+    "revision": "10b3349b5d345aab41d1",
+    "url": "/ReactLearn/static/js/runtime~main.10b3349b.js"
+  },
+  {
+    "revision": "795e4fe55a3418cd900a8b12ffb82d85",
+    "url": "/ReactLearn/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bbb11e7785cf41cd2fa564562dcf48e4.js"
+  "/ReactLearn/precache-manifest.ff2be0fc67f610a3af51639b15028a63.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ReactLearn/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
